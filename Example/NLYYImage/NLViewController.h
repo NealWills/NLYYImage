83c55027f1170dc88600bb13b6dee6d16baa017d
NealWills <nealwills93@gmail.com>
2025-03-05 09:25:02 +0800

    Swift Change To ObjC

---
+++ Example/NLYYImage/NLViewController.h
@@ -0,0 +1,13 @@
+//
+//  NLViewController.h
+//  NLYYImage
+//
+//  Created by NealWills on 03/05/2025.
+//  Copyright (c) 2025 NealWills. All rights reserved.
+//
+
+@import UIKit;
+
+@interface NLViewController : UIViewController
+
+@end
---
+++ Example/Pods/Target Support Files/Pods-NLYYImage_Tests/Pods-NLYYImage_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_NLYYImage_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_NLYYImage_Tests
+@end
